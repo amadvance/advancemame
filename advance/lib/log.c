ad642548f639e189a4308074f4ead6c29c1e823d
Andrea Mazzoleni <amadvance@gmail.com>
2002-08-02 17:30:58 +0000

    Commit

---
+++ advance/lib/log.c
@@ -0,0 +1,90 @@
+/*
+ * This file is part of the AdvanceMAME project.
+ *
+ * Copyright (C) 2002 Andrea Mazzoleni
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * In addition, as a special exception, Andrea Mazzoleni
+ * gives permission to link the code of this program with
+ * the MAME library (or with modified versions of MAME that use the
+ * same license as MAME), and distribute linked combinations including
+ * the two.  You must obey the GNU General Public License in all
+ * respects for all of the code used other than MAME.  If you modify
+ * this file, you may extend this exception to your version of the
+ * file, but you are not obligated to do so.  If you do not wish to
+ * do so, delete this exception statement from your version.
+ */
+
+#include "log.h"
+#include "os.h"
+
+#include <stdio.h>
+
+/***************************************************************************/
+/* Log */
+
+struct log_context {
+	FILE* msg;
+	int msg_sync_flag;
+};
+
+static struct log_context LOG;
+
+void log_va(const char *text, va_list arg) {
+	if (LOG.msg) {
+		vfprintf(LOG.msg,text,arg);
+
+		if (LOG.msg_sync_flag) {
+			fflush(LOG.msg);
+			target_sync();
+		}
+	}
+}
+
+void log_f(const char *text, ...) {
+	va_list arg;
+	va_start(arg, text);
+	log_va(text, arg);
+	va_end(arg);
+}
+
+int log_init(const char* file, int sync_flag) {
+
+	LOG.msg_sync_flag = sync_flag;
+	LOG.msg = 0;
+
+	if (file) {
+		LOG.msg = fopen(file,"w");
+		if (!LOG.msg)
+			return -1;
+	}
+
+	return 0;
+}
+
+void log_done(void) {
+	if (LOG.msg) {
+		fclose(LOG.msg);
+		LOG.msg = 0;
+	}
+}
+
+void log_abort(void) {
+	if (LOG.msg) {
+		fclose(LOG.msg);
+		LOG.msg = 0;
+	}
+}
---
+++ advance/lib/log.h
@@ -0,0 +1,64 @@
+/*
+ * This file is part of the AdvanceMAME project.
+ *
+ * Copyright (C) 2002 Andrea Mazzoleni
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * In addition, as a special exception, Andrea Mazzoleni
+ * gives permission to link the code of this program with
+ * the MAME library (or with modified versions of MAME that use the
+ * same license as MAME), and distribute linked combinations including
+ * the two.  You must obey the GNU General Public License in all
+ * respects for all of the code used other than MAME.  If you modify
+ * this file, you may extend this exception to your version of the
+ * file, but you are not obligated to do so.  If you do not wish to
+ * do so, delete this exception statement from your version.
+ */
+
+#ifndef __LOG_H
+#define __LOG_H
+
+#include <stdarg.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***************************************************************************/
+/* Log */
+
+int log_init(const char* file, int sync_flag);
+void log_done(void);
+void log_abort(void);
+
+void log_va(const char *text, va_list arg);
+void log_f(const char *text, ...);
+
+#ifndef NDEBUG
+#define log_std(a) log_f a
+#define log_debug(a) log_f a
+#define log_pedantic(a) do { } while (0)
+#else
+#define log_std(a) log_f a
+#define log_debug(a) do { } while (0)
+#define log_pedantic(a) do { } while (0)
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
