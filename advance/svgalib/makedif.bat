d0ab17c50957003b35387e1dd1db04618966432d
Andrea Mazzoleni <amadvance@gmail.com>
2003-06-17 18:45:43 +0000

    Commit

--- advance/svgalib/makedif.bat
+++
@@ -1,8 +0,0 @@
-set src=/mame/svgalib
-
-diff -u %src% . | grep -v "^Only" | grep -v "^Common" | grep -v "^Binary" > svgalib.dif
-diff -u %src%/drivers drivers | grep -v "^Only" | grep -v "^Common" | grep -v "^Binary" >> svgalib.dif
-diff -u %src%/ramdac ramdac | grep -v "^Only" | grep -v "^Common" | grep -v "^Binary" >> svgalib.dif
-diff -u %src%/clockchi clockchi | grep -v "^Only" | grep -v "^Common" | grep -v "^Binary" >> svgalib.dif
-
-set src=
