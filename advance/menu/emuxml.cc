fd4148b36c6b4edb3352742c136a50bff05c4abb
Andrea Mazzoleni <amadvance@gmail.com>
2003-08-21 21:51:08 +0000

    Commit

--- advance/menu/emuxml.cc
+++ advance/menu/emuxml.cc
@@ -58,7 +58,7 @@ struct state_t {
 
 static void process_error(struct state_t* state, const char* tag, const char* msg)
 {
-	if (*tag)
+	if (tag)
 		target_err("Error reading at line %d for element/attribute `%s' for %s.\n", XML_GetCurrentLineNumber(state->parser), tag, msg);
 	else
 		target_err("Error reading at line %d for %s.\n", XML_GetCurrentLineNumber(state->parser), msg);
