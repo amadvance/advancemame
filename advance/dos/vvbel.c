3af3beddc6fd3199c3acb9fa9986922abc5c32a8
Andrea Mazzoleni <amadvance@gmail.com>
2009-01-11 16:13:42 +0000

    Commit

--- advance/dos/vvbel.c
+++ advance/dos/vvbel.c
@@ -722,10 +722,10 @@ adv_error vbeline_mode_import(adv_mode* mode, const vbeline_video_mode* vbeline_
 	}
 
 	mode->driver = &video_vbeline_driver;
-	mode->flags = MODE_FLAGS_RETRACE_WAIT_SYNC | MODE_FLAGS_RETRACE_SET_ASYNC
+	mode->flags = MODE_FLAGS_RETRACE_WAIT_SYNC | MODE_FLAGS_RETRACE_SET_SYNC
 		| (mode->flags & MODE_FLAGS_USER_MASK);
 	if ((info.ModeAttributes & vbeMdTripleBuffer) != 0)
-		mode->flags |= MODE_FLAGS_SCROLL_SYNC;
+		mode->flags |= MODE_FLAGS_RETRACE_SET_ASYNC;
 	switch (info.MemoryModel) {
 		case vbeMemTXT :
 			mode->flags |= MODE_FLAGS_INDEX_TEXT;
