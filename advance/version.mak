cf1282059d084a6412ef05e48659922aea64f77e
Andrea Mazzoleni <amadvance@gmail.com>
2015-08-06 22:22:02 +0200

    Change emulator version to v1.4

--- advance/version.mak
+++ advance/version.mak
@@ -1,7 +1,7 @@
 ############################################################################
 # Version
 
-EMUVERSION = 1.3
+EMUVERSION = 1.4
 MENUVERSION = 2.8
 CABVERSION = 1.1.4
 
--- doc/histemu.d
+++ doc/histemu.d
@@ -1,6 +1,9 @@
 Name
 	history - History For AdvanceMAME/MESS
 
+AdvanceMAME/MESS Version 1.4 2015/08
+	) Restored the missing keypress on some games, like pengo.
+
 AdvanceMAME/MESS Version 1.3 2015/06
 	) Added support fo Haiku OS.
 	) Included free ROMs gridlee, polyplay and robby.
