5e5607bc1148df98c8ef38fae96423db258f1cbe
Andrea Mazzoleni <amadvance@gmail.com>
2016-12-04 15:33:07 +0100

    Update makedeb

--- makedeb.sh
+++ makedeb.sh
@@ -4,6 +4,7 @@
 # Reconfigure (with force) to get the latest revision from git
 autoreconf -f
 
+# Build for a generic architecture not using -march
 if ! ./configure CFLAGS="-O2 -fno-strict-aliasing -fno-strict-overflow -fsigned-char"; then
 	exit 1
 fi
