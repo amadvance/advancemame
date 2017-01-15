da1ad67ee20e218a46c930087cf6a1018c944007
Andrea Mazzoleni <amadvance@gmail.com>
2017-01-15 19:47:34 +0100

    Add more image file extensions for the Sega Master System

--- mess/systems/sms.c
+++ mess/systems/sms.c
@@ -315,7 +315,7 @@ static void sms_cartslot_getinfo(const device_class *devclass, UINT32 state, uni
 		case DEVINFO_PTR_LOAD:				info->load = device_load_sms_cart; break;
 
 		/* --- the following bits of info are returned as NULL-terminated strings --- */
-		case DEVINFO_STR_FILE_EXTENSIONS:		strcpy(info->s = device_temp_str(), "sms"); break;
+		case DEVINFO_STR_FILE_EXTENSIONS:		strcpy(info->s = device_temp_str(), "sms,bin,ic1"); break;
 
 		default:					cartslot_device_getinfo(devclass, state, info); break;
 	}
