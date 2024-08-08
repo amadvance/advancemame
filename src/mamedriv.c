870242ba7820775e73d9c678faa00c0a03c9f5c5
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2024-08-08 19:29:08 +0100

    New ones

--- src/mamedriv.c
+++ src/mamedriv.c
@@ -2258,6 +2258,7 @@ V-V                           TP-027
 	DRIVER( madgear )	/*  2/1989 (c) 1989 (US) */
 	DRIVER( madgearj )	/*  2/1989 (c) 1989 (Japan) */
 	DRIVER( ledstorm )	/*  2/1989 (c) 1989 (US) */
+	DRIVER( leds2011 )     /*  2/1988 (c) 1988 (World) */
 	/*  3/1989 Dokaben (baseball) - see below among "Mitchell" games */
 	/*  8/1989 Dokaben 2 (baseball) - see below among "Mitchell" games */
 	/* 10/1989 Capcom Baseball - see below among "Mitchell" games */
@@ -2295,6 +2296,7 @@ V-V                           TP-027
 	DRIVER( ffightj1 )	/* 12/1989 (c) (Japan) */
 	DRIVER( 1941 )		/*  2/1990 (c) 1990 (World) */
 	DRIVER( 1941j )		/*  2/1990 (c) 1990 (Japan) */
+	DRIVER( 1943mii )       /*  6/1987 (c) 1987 (US) */
 	DRIVER( mercs )		/* 02/03/1990 (c) 1990 (World) */
 	DRIVER( mercsu )	/* 02/03/1990 (c) 1990 (US)    */
 	DRIVER( mercsua )	/* 08/06/1990 (c) 1990 (US)    */
