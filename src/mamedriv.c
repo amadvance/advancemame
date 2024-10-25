b96ccd4e11ba06773f3806b9c91c3c1402a98dbf
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2024-10-25 10:24:30 +0100

    sbubsm (#172)

--- src/mamedriv.c
+++ src/mamedriv.c
@@ -3629,6 +3629,7 @@ BOMULEUL CHAJARA SEGA ST-V  1997/04/11
 	/* Genie Hardware (uses Genesis VDP) also has 'Sun Mixing Co' put into tile ram */
 	DRIVER( puckpkmn )	/* (c) 2000 Genie */
 	DRIVER( topshoot )	/* (c) 1995 Sun Mixing */
+	DRIVER( sbubsm )	/* (c) 1996 Sun Mixing */
 
 	/* Sega MegaTech, the number shown is on the label of the instruction rom */
  	DRIVER( megatech )
