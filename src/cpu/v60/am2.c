2a22d267524e11abcaae5eef410e2fdda6e62cf2
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2025-01-13 14:40:51 +0000

    F1 Superlap needs this either a bug or extra protection.?? (#181)

--- src/cpu/v60/am2.c
+++ src/cpu/v60/am2.c
@@ -956,8 +956,9 @@ static UINT32 am2ImmediateQuick(void)
 
 static UINT32 am2Error1(void)
 {
-	fatalerror("CPU - AM2 - 1 (PC=%06x)", PC);
-	return 0; /* never reached, fatalerror won't return */
+	/* f1lap trips this, why? */
+	logerror("CPU - AM2 - 1 (PC=%06x)", PC);
+	return 0;
 }
 
 static UINT32 am2Error2(void)
