920ee8da45d5c46123ab54e1ebec8b99d00262d4
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2025-01-27 22:39:25 +0000

    Hooks for the samples

--- src/sndhrdw/irem.h
+++ src/sndhrdw/irem.h
@@ -3,7 +3,10 @@
     Irem audio interface
 
 ****************************************************************************/
+#include "sound/samples.h"
 
 MACHINE_DRIVER_EXTERN( irem_audio );
 
 WRITE8_HANDLER( irem_sound_cmd_w );
+
+extern struct Samplesinterface tr606_samples_interface;
