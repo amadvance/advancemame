70a5e872898d84b17df834776fd617ec364e2c7e
Andrea Mazzoleni <amadvance@gmail.com>
2025-03-17 16:39:42 +0100

    Backport to MAME 0.106 format

--- src/drivers/snesb.c
+++ src/drivers/snesb.c
@@ -82,6 +82,8 @@ TODO:
 #include "driver.h"
 #include "includes/snes.h"
 
+extern DRIVER_INIT( snes );
+
 static INT8 *shared_ram;
 static UINT8 ffight2b_coins;
 
@@ -89,7 +91,7 @@ static READ8_HANDLER(sharedram_r)
 {
 	static INT32 oldinput=0;
 	INT32 coincnt;
-	INT32 input = input_port_read(machine, "COIN");
+	INT32 input = readinputport(13); // input_port_read(machine, "COIN");
 
 	if(input&3)
 	{
@@ -114,7 +116,7 @@ static WRITE8_HANDLER(sharedram_w)
 static READ8_HANDLER(ffight2b_coin_r)
 {
 	static INT32 oldinput=0;
-	INT32 input = input_port_read(machine, "COIN");
+	INT32 input = readinputport(13); // input_port_read(machine, "COIN");
 
 	if( ((input&1)==1)&&((oldinput&1)==0))
 	{
@@ -129,7 +131,7 @@ static READ8_HANDLER(ffight2b_coin_r)
 static ADDRESS_MAP_START( snesb_map, ADDRESS_SPACE_PROGRAM, 8)
 	AM_RANGE(0x000000, 0x2fffff) AM_READWRITE(snes_r_bank1, snes_w_bank1)	/* I/O and ROM (repeats for each bank) */
 	AM_RANGE(0x300000, 0x3fffff) AM_READWRITE(snes_r_bank2, snes_w_bank2)	/* I/O and ROM (repeats for each bank) */
-	AM_RANGE(0x400000, 0x5fffff) AM_READWRITE(snes_r_bank3, SMH_ROM)	/* ROM (and reserved in Mode 20) */
+	AM_RANGE(0x400000, 0x5fffff) AM_READWRITE(snes_r_bank3, MWA8_ROM)	/* ROM (and reserved in Mode 20) */
 	AM_RANGE(0x600000, 0x6fffff) AM_READWRITE(snes_r_bank6, snes_w_bank6)	/* used by Mode 20 DSP-1 */
 	AM_RANGE(0x700000, 0x77ffff) AM_READWRITE(snes_r_sram, snes_w_sram)	/* 256KB Mode 20 save ram + reserved from 0x8000 - 0xffff */
 	AM_RANGE(0x7e0000, 0x7fffff) AM_RAM					/* 8KB Low RAM, 24KB High RAM, 96KB Expanded RAM */
@@ -138,12 +140,12 @@ ADDRESS_MAP_END
 
 static READ8_HANDLER( spc_ram_100_r )
 {
-	return spc_ram_r(machine, offset + 0x100);
+	return spc_ram_r(offset + 0x100);
 }
 
 static WRITE8_HANDLER( spc_ram_100_w )
 {
-	spc_ram_w(machine, offset + 0x100, data);
+	spc_ram_w(offset + 0x100, data);
 }
 
 static ADDRESS_MAP_START( spc_mem, ADDRESS_SPACE_PROGRAM, 8 )
@@ -155,12 +157,12 @@ static ADDRESS_MAP_START( spc_mem, ADDRESS_SPACE_PROGRAM, 8 )
 ADDRESS_MAP_END
 
 static INPUT_PORTS_START( kinstb )
-	PORT_START("PAD1L")		/* IN 0 : Joypad 1 - L */
+	PORT_START		/* IN 0 : Joypad 1 - L */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON3 ) PORT_NAME("P1 Button A") PORT_PLAYER(1)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_NAME("P1 Button X") PORT_PLAYER(1)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_BUTTON5 ) PORT_NAME("P1 Button L") PORT_PLAYER(1)
 	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_BUTTON6 ) PORT_NAME("P1 Button R") PORT_PLAYER(1)
-	PORT_START("PAD1H")		/* IN 1 : Joypad 1 - H */
+	PORT_START		/* IN 1 : Joypad 1 - H */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("P1 Button B") PORT_PLAYER(1)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_NAME("P1 Button Y") PORT_PLAYER(1)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_SERVICE1 ) PORT_NAME("P1 Select")
@@ -170,12 +172,12 @@ static INPUT_PORTS_START( kinstb )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(1)
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(1)
 
-	PORT_START("PAD2L")		/* IN 2 : Joypad 2 - L */
+	PORT_START		/* IN 2 : Joypad 2 - L */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON3 ) PORT_NAME("P2 Button A") PORT_PLAYER(2)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_NAME("P2 Button X") PORT_PLAYER(2)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_BUTTON5 ) PORT_NAME("P2 Button L") PORT_PLAYER(2)
 	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_BUTTON6 ) PORT_NAME("P2 Button R") PORT_PLAYER(2)
-	PORT_START("PAD2H")		/* IN 3 : Joypad 2 - H */
+	PORT_START		/* IN 3 : Joypad 2 - H */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("P2 Button B") PORT_PLAYER(2)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_NAME("P2 Button Y") PORT_PLAYER(2)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_SERVICE2 ) PORT_NAME("P2 Select")
@@ -185,12 +187,12 @@ static INPUT_PORTS_START( kinstb )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(2)
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(2)
 
-	PORT_START("PAD3L")		/* IN 4 : Joypad 3 - L */
+	PORT_START		/* IN 4 : Joypad 3 - L */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON3 ) PORT_NAME("P3 Button A") PORT_PLAYER(3)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_NAME("P3 Button X") PORT_PLAYER(3)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_BUTTON5 ) PORT_NAME("P3 Button L") PORT_PLAYER(3)
 	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_BUTTON6 ) PORT_NAME("P3 Button R") PORT_PLAYER(3)
-	PORT_START("PAD3H")		/* IN 5 : Joypad 3 - H */
+	PORT_START		/* IN 5 : Joypad 3 - H */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("P3 Button B") PORT_PLAYER(3)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_NAME("P3 Button Y") PORT_PLAYER(3)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_SERVICE3 ) PORT_NAME("P3 Select")
@@ -200,12 +202,12 @@ static INPUT_PORTS_START( kinstb )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(3)
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(3)
 
-	PORT_START("PAD4L")		/* IN 6 : Joypad 4 - L */
+	PORT_START		/* IN 6 : Joypad 4 - L */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON3 ) PORT_NAME("P4 Button A") PORT_PLAYER(4)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_NAME("P4 Button X") PORT_PLAYER(4)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_BUTTON5 ) PORT_NAME("P4 Button L") PORT_PLAYER(4)
 	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_BUTTON6 ) PORT_NAME("P4 Button R") PORT_PLAYER(4)
-	PORT_START("PAD4H")		/* IN 7 : Joypad 4 - H */
+	PORT_START		/* IN 7 : Joypad 4 - H */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("P4 Button B") PORT_PLAYER(4)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_NAME("P4 Button Y") PORT_PLAYER(4)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_SERVICE4 ) PORT_NAME("P4 Select")
@@ -215,13 +217,13 @@ static INPUT_PORTS_START( kinstb )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(4)
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(4)
 
-	PORT_START("INTERNAL")		/* IN 8 : Internal switches */
+	PORT_START		/* IN 8 : Internal switches */
 	PORT_DIPNAME( 0x1, 0x1, "Enforce 32 sprites/line" )
 	PORT_DIPSETTING(   0x0, DEF_STR( No )  )
 	PORT_DIPSETTING(   0x1, DEF_STR( Yes ) )
 
 #ifdef MAME_DEBUG
-	PORT_START("DEBUG1")	/* IN 9 : debug switches */
+	PORT_START	/* IN 9 : debug switches */
 	PORT_DIPNAME( 0x3, 0x0, "Browse tiles" )
 	PORT_DIPSETTING(   0x0, DEF_STR( Off ) )
 	PORT_DIPSETTING(   0x1, "2bpl"  )
@@ -233,7 +235,7 @@ static INPUT_PORTS_START( kinstb )
 	PORT_DIPSETTING(   0x8, "4bpl"  )
 	PORT_DIPSETTING(   0xc, "8bpl"  )
 
-	PORT_START("DEBUG2")	/* IN 10 : debug switches */
+	PORT_START	/* IN 10 : debug switches */
 	PORT_BIT( 0x1, IP_ACTIVE_HIGH, IPT_BUTTON7 ) PORT_NAME("Toggle BG 1") PORT_PLAYER(2)
 	PORT_BIT( 0x2, IP_ACTIVE_HIGH, IPT_BUTTON8 ) PORT_NAME("Toggle BG 2") PORT_PLAYER(2)
 	PORT_BIT( 0x4, IP_ACTIVE_HIGH, IPT_BUTTON9 ) PORT_NAME("Toggle BG 3") PORT_PLAYER(2)
@@ -243,15 +245,19 @@ static INPUT_PORTS_START( kinstb )
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON9 ) PORT_NAME("Toggle Back col") PORT_PLAYER(3)
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON10 ) PORT_NAME("Toggle Windows") PORT_PLAYER(3)
 
-	PORT_START("DEBUG3")	/* IN 11 : debug input */
+	PORT_START	/* IN 11 : debug input */
 	PORT_BIT( 0x1, IP_ACTIVE_HIGH, IPT_BUTTON9 ) PORT_NAME("Pal prev")
 	PORT_BIT( 0x2, IP_ACTIVE_HIGH, IPT_BUTTON10 ) PORT_NAME("Pal next")
 	PORT_BIT( 0x4, IP_ACTIVE_HIGH, IPT_BUTTON7 ) PORT_NAME("Toggle Transparency") PORT_PLAYER(4)
+#else
+	PORT_START	/* IN 9 : debug switches */
+	PORT_START	/* IN 10 : debug switches */
+	PORT_START	/* IN 11 : debug input */
 #endif
 
-	PORT_START("DSW")	/* IN 12 : dip-switches */
+	PORT_START	/* IN 12 : dip-switches */
 
-	PORT_START("COIN")	/* IN 13 : coins */
+	PORT_START	/* IN 13 : coins */
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_COIN1 )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_COIN2 )
 
@@ -259,12 +265,12 @@ INPUT_PORTS_END
 
 
 static INPUT_PORTS_START( ffight2b )
-	PORT_START("PAD1L")		/* IN 0 : Joypad 1 - L */
+	PORT_START		/* IN 0 : Joypad 1 - L */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON3 ) PORT_NAME("P1 Button A") PORT_PLAYER(1)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_NAME("P1 Button X") PORT_PLAYER(1)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_BUTTON5 ) PORT_NAME("P1 Button L") PORT_PLAYER(1)
 	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_BUTTON6 ) PORT_NAME("P1 Button R") PORT_PLAYER(1)
-	PORT_START("PAD1H")		/* IN 1 : Joypad 1 - H */
+	PORT_START		/* IN 1 : Joypad 1 - H */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("P1 Button B") PORT_PLAYER(1)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_NAME("P1 Button Y") PORT_PLAYER(1)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_SERVICE1 ) PORT_NAME("P1 Select")
@@ -274,12 +280,12 @@ static INPUT_PORTS_START( ffight2b )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(1)
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(1)
 
-	PORT_START("PAD2L")		/* IN 2 : Joypad 2 - L */
+	PORT_START		/* IN 2 : Joypad 2 - L */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON3 ) PORT_NAME("P2 Button A") PORT_PLAYER(2)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_NAME("P2 Button X") PORT_PLAYER(2)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_BUTTON5 ) PORT_NAME("P2 Button L") PORT_PLAYER(2)
 	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_BUTTON6 ) PORT_NAME("P2 Button R") PORT_PLAYER(2)
-	PORT_START("PAD2H")		/* IN 3 : Joypad 2 - H */
+	PORT_START		/* IN 3 : Joypad 2 - H */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("P2 Button B") PORT_PLAYER(2)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_NAME("P2 Button Y") PORT_PLAYER(2)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_SERVICE2 ) PORT_NAME("P2 Select")
@@ -289,12 +295,12 @@ static INPUT_PORTS_START( ffight2b )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(2)
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(2)
 
-	PORT_START("PAD3L")		/* IN 4 : Joypad 3 - L */
+	PORT_START		/* IN 4 : Joypad 3 - L */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON3 ) PORT_NAME("P3 Button A") PORT_PLAYER(3)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_NAME("P3 Button X") PORT_PLAYER(3)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_BUTTON5 ) PORT_NAME("P3 Button L") PORT_PLAYER(3)
 	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_BUTTON6 ) PORT_NAME("P3 Button R") PORT_PLAYER(3)
-	PORT_START("PAD3H")		/* IN 5 : Joypad 3 - H */
+	PORT_START		/* IN 5 : Joypad 3 - H */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("P3 Button B") PORT_PLAYER(3)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_NAME("P3 Button Y") PORT_PLAYER(3)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_SERVICE3 ) PORT_NAME("P3 Select")
@@ -304,12 +310,12 @@ static INPUT_PORTS_START( ffight2b )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(3)
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(3)
 
-	PORT_START("PAD4L")		/* IN 6 : Joypad 4 - L */
+	PORT_START		/* IN 6 : Joypad 4 - L */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON3 ) PORT_NAME("P4 Button A") PORT_PLAYER(4)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_NAME("P4 Button X") PORT_PLAYER(4)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_BUTTON5 ) PORT_NAME("P4 Button L") PORT_PLAYER(4)
 	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_BUTTON6 ) PORT_NAME("P4 Button R") PORT_PLAYER(4)
-	PORT_START("PAD4H")		/* IN 7 : Joypad 4 - H */
+	PORT_START		/* IN 7 : Joypad 4 - H */
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("P4 Button B") PORT_PLAYER(4)
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_NAME("P4 Button Y") PORT_PLAYER(4)
 	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_SERVICE4 ) PORT_NAME("P4 Select")
@@ -319,13 +325,13 @@ static INPUT_PORTS_START( ffight2b )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(4)
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(4)
 
-	PORT_START("INTERNAL")		/* IN 8 : Internal switches */
+	PORT_START		/* IN 8 : Internal switches */
 	PORT_DIPNAME( 0x1, 0x1, "Enforce 32 sprites/line" )
 	PORT_DIPSETTING(   0x0, DEF_STR( No )  )
 	PORT_DIPSETTING(   0x1, DEF_STR( Yes ) )
 
 #ifdef MAME_DEBUG
-	PORT_START("DEBUG1")	/* IN 9 : debug switches */
+	PORT_START	/* IN 9 : debug switches */
 	PORT_DIPNAME( 0x3, 0x0, "Browse tiles" )
 	PORT_DIPSETTING(   0x0, DEF_STR( Off ) )
 	PORT_DIPSETTING(   0x1, "2bpl"  )
@@ -337,7 +343,7 @@ static INPUT_PORTS_START( ffight2b )
 	PORT_DIPSETTING(   0x8, "4bpl"  )
 	PORT_DIPSETTING(   0xc, "8bpl"  )
 
-	PORT_START("DEBUG2")	/* IN 10 : debug switches */
+	PORT_START	/* IN 10 : debug switches */
 	PORT_BIT( 0x1, IP_ACTIVE_HIGH, IPT_BUTTON7 ) PORT_NAME("Toggle BG 1") PORT_PLAYER(2)
 	PORT_BIT( 0x2, IP_ACTIVE_HIGH, IPT_BUTTON8 ) PORT_NAME("Toggle BG 2") PORT_PLAYER(2)
 	PORT_BIT( 0x4, IP_ACTIVE_HIGH, IPT_BUTTON9 ) PORT_NAME("Toggle BG 3") PORT_PLAYER(2)
@@ -347,13 +353,17 @@ static INPUT_PORTS_START( ffight2b )
 	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON9 ) PORT_NAME("Toggle Back col") PORT_PLAYER(3)
 	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_BUTTON10 ) PORT_NAME("Toggle Windows") PORT_PLAYER(3)
 
-	PORT_START("DEBUG3")	/* IN 11 : debug input */
+	PORT_START	/* IN 11 : debug input */
 	PORT_BIT( 0x1, IP_ACTIVE_HIGH, IPT_BUTTON9 ) PORT_NAME("Pal prev")
 	PORT_BIT( 0x2, IP_ACTIVE_HIGH, IPT_BUTTON10 ) PORT_NAME("Pal next")
 	PORT_BIT( 0x4, IP_ACTIVE_HIGH, IPT_BUTTON7 ) PORT_NAME("Toggle Transparency") PORT_PLAYER(4)
+#else
+	PORT_START	/* IN 9 : debug switches */
+	PORT_START	/* IN 10 : debug switches */
+	PORT_START	/* IN 11 : debug input */
 #endif
 
-	PORT_START("DSW")	/* IN 12 : dip-switches */
+	PORT_START	/* IN 12 : dip-switches */
 	PORT_DIPNAME( 0x01, 0x01, "Player Bonus" ) PORT_DIPLOCATION("SW1:1")
 	PORT_DIPSETTING(    0x01, DEF_STR( No ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( Yes ) )
@@ -377,41 +387,70 @@ static INPUT_PORTS_START( ffight2b )
 	PORT_DIPSETTING(    0xa0, DEF_STR( 1C_3C ) )
 	PORT_DIPSETTING(    0x80, DEF_STR( 1C_4C ) )
 
-	PORT_START("COIN")	/* IN 13 : coins */
+	PORT_START	/* IN 13 : coins */
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_COIN1 )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_COIN2 )
 
 INPUT_PORTS_END
 
-static const custom_sound_interface snes_sound_interface =
+static struct CustomSound_interface snes_sound_interface =
 { snes_sh_start };
+
+static const gfx_decode gfxdecodeinfo[] =
+{
+	{ -1 } /* end of array */
+};
+
+static PALETTE_INIT( snes )
+{
+	int i, r, g, b;
+
+	for( i = 0; i < 32768; i++ )
+	{
+		r = (i & 0x1F) << 3;
+		g = ((i >> 5) & 0x1F) << 3;
+		b = ((i >> 10) & 0x1F) << 3;
+		palette_set_color( i, r, g, b );
+	}
+
+	/* The colortable can be black */
+	for( i = 0; i < 256; i++ )
+		colortable[i] = 0;
+}
 
 static MACHINE_DRIVER_START( kinstb )
 	/* basic machine hardware */
-	MDRV_CPU_ADD("main", G65816, 3580000)	/* 2.68Mhz, also 3.58Mhz */
+	MDRV_CPU_ADD_TAG("main", G65816, 3580000)	/* 2.68Mhz, also 3.58Mhz */
 	MDRV_CPU_PROGRAM_MAP(snesb_map, 0)
 
 	/* audio CPU */
-	MDRV_CPU_ADD("sound", SPC700, 2048000/2)	/* 2.048 Mhz, but internal divider */
+	MDRV_CPU_ADD_TAG("sound", SPC700, 2048000/2)	/* 2.048 Mhz, but internal divider */
 	MDRV_CPU_PROGRAM_MAP(spc_mem, 0)
+	MDRV_CPU_VBLANK_INT(NULL, 0)
 
+	MDRV_FRAMES_PER_SECOND(60)
+	MDRV_VBLANK_DURATION((int)(((262. - 240.) / 262.) * 1000000. / 60.))
 	MDRV_INTERLEAVE(400)
 
 	MDRV_MACHINE_START( snes )
 	MDRV_MACHINE_RESET( snes )
 
 	/* video hardware */
+	MDRV_VIDEO_START( generic_bitmapped )
 	MDRV_VIDEO_UPDATE( snes )
 
-
-	MDRV_SCREEN_ADD("main", RASTER)
-	MDRV_SCREEN_FORMAT(BITMAP_FORMAT_RGB32)
-	MDRV_SCREEN_RAW_PARAMS(DOTCLK_NTSC, SNES_HTOTAL, 0, SNES_SCR_WIDTH, SNES_VTOTAL_NTSC, 0, SNES_SCR_HEIGHT_NTSC)
+	MDRV_VIDEO_ATTRIBUTES(VIDEO_TYPE_RASTER)
+	MDRV_SCREEN_SIZE(SNES_SCR_WIDTH * 2, SNES_SCR_HEIGHT * 2)
+	MDRV_VISIBLE_AREA(0, SNES_SCR_WIDTH*2-1, 0, SNES_SCR_HEIGHT*2-1 )
+	MDRV_GFXDECODE(gfxdecodeinfo)
+	MDRV_PALETTE_LENGTH(32768)
+	MDRV_COLORTABLE_LENGTH(257)
+	MDRV_PALETTE_INIT( snes )
 
 	/* sound hardware */
 	MDRV_SPEAKER_STANDARD_STEREO("left", "right")
 
-	MDRV_SOUND_ADD("snes", CUSTOM, 0)
+	MDRV_SOUND_ADD(CUSTOM, 0)
 	MDRV_SOUND_CONFIG(snes_sound_interface)
 	MDRV_SOUND_ROUTE(0, "left", 1.00)
 	MDRV_SOUND_ROUTE(1, "right", 1.00)
@@ -420,7 +459,7 @@ MACHINE_DRIVER_END
 static DRIVER_INIT(kinstb)
 {
 	INT32 i;
-	UINT8 *rom = memory_region(machine, "user3");
+	UINT8 *rom = memory_region(REGION_USER3);
 
 	for(i=0;i<0x400000;i++)
 	{
@@ -428,15 +467,18 @@ static DRIVER_INIT(kinstb)
 	}
 
 	shared_ram=auto_malloc(0x100);
-	memory_install_readwrite8_handler(machine, 0, ADDRESS_SPACE_PROGRAM, 0x781000, 0x7810ff, 0, 0, sharedram_r, sharedram_w);
 
-	DRIVER_INIT_CALL(snes_hirom);
+	memory_install_read8_handler(0, ADDRESS_SPACE_PROGRAM, 0x781000, 0x7810ff, 0, 0, sharedram_r);
+	memory_install_write8_handler(0, ADDRESS_SPACE_PROGRAM, 0x781000, 0x7810ff, 0, 0, sharedram_w);
+
+	// DISABLED FOR NOW AS NOT REQUIRED FOR ffight2b
+	//DRIVER_INIT_CALL(snes_hirom);
 }
 
 DRIVER_INIT( ffight2b )
 {
 	INT32 i;
-	UINT8 *rom = memory_region(machine, "user3");
+	UINT8 *rom = memory_region(REGION_USER3);
 
 	for(i=0;i<0x200000;i++)
 	{
@@ -470,27 +512,28 @@ DRIVER_INIT( ffight2b )
 	rom[0x7ffc]=0x54;
 
 	ffight2b_coins=0;
-	memory_install_read8_handler(machine, 0, ADDRESS_SPACE_PROGRAM, 0x7eadce, 0x7eadce, 0, 0, ffight2b_coin_r);
+	memory_install_read8_handler(0, ADDRESS_SPACE_PROGRAM, 0x7eadce, 0x7eadce, 0, 0, ffight2b_coin_r);
 
-	DRIVER_INIT_CALL(snes);
+	//DRIVER_INIT_CALL(snes);
+	init_snes();
 }
 
 ROM_START( kinstb )
-	ROM_REGION( 0x400000, "user3", ROMREGION_DISPOSE )
+	ROM_REGION( 0x400000, REGION_USER3, ROMREGION_DISPOSE )
 	ROM_LOAD( "1.u14", 0x000000, 0x100000, CRC(70889919) SHA1(1451714cbdacb7f6ced2bc7afa478ad7264cf3b7) )
 	ROM_LOAD( "2.u15", 0x100000, 0x100000, CRC(e4a5d1da) SHA1(6ae566bd2f740a251d7a81b8ebb92a651cfaac8d) )
 	ROM_LOAD( "3.u16", 0x200000, 0x100000, CRC(7a40f7dd) SHA1(cebe632e8d2d68d0619077cc1e931af73c9a723b) )
 	ROM_LOAD( "4.u17", 0x300000, 0x100000, CRC(3d7564c1) SHA1(392b513991897668d5dd469ac84a34f785895774) )
 
-	ROM_REGION(0x100,           "user5", 0)
+	ROM_REGION(0x100,           REGION_USER5, 0)
 	ROM_LOAD("spc700.rom", 0, 0x40, CRC(44bb3a40) SHA1(97e352553e94242ae823547cd853eecda55c20f0) )
 
-	ROM_REGION(0x800,           "user6", ROMREGION_ERASEFF)
+	ROM_REGION(0x800,           REGION_USER6, ROMREGION_ERASEFF)
 
 ROM_END
 
 ROM_START( ffight2b )
-	ROM_REGION( 0x400000, "user3", ROMREGION_DISPOSE )
+	ROM_REGION( 0x400000, REGION_USER3, ROMREGION_DISPOSE )
 	ROM_LOAD( "ff2_3.u6", 0x000000, 0x008000, CRC(343bf582) SHA1(cc6b7219bb2fe61f0b377b606ad28b0e5a78be0b) )
 	ROM_CONTINUE(          0x088000, 0x008000 )
 	ROM_CONTINUE(          0x010000, 0x008000 )
@@ -525,24 +568,24 @@ ROM_START( ffight2b )
 	ROM_CONTINUE(          0x078000, 0x008000 )
 	ROM_LOAD( "ff2_1.u8", 0x100000, 0x040000, CRC(ea315ac1) SHA1(a85de091882d35bc77dc99677511828ff7c20350) )
 
-	ROM_REGION(0x100,           "user5", 0)
+	ROM_REGION(0x100,           REGION_USER5, 0)
 	ROM_LOAD("spc700.rom", 0, 0x40, CRC(44bb3a40) SHA1(97e352553e94242ae823547cd853eecda55c20f0) )
 
-	ROM_REGION(0x800,           "user6", ROMREGION_ERASEFF)
+	ROM_REGION(0x800,           REGION_USER6, ROMREGION_ERASEFF)
 
 ROM_END
 
 ROM_START( sblast2b )
-	ROM_REGION( 0x180000, "user3", ROMREGION_DISPOSE )
+	ROM_REGION( 0x180000, REGION_USER3, ROMREGION_DISPOSE )
 	ROM_LOAD( "1.bin", 0x000000, 0x080000, CRC(bea10c40) SHA1(d9cc65267b9b57145d714f2c17b436c1fb21513f) )
 	ROM_LOAD( "2.bin", 0x080000, 0x080000, CRC(57d2b6e9) SHA1(1a7b347101f67b254e2f86294d501b0669431644) )
 	ROM_LOAD( "3.bin", 0x100000, 0x080000, CRC(9e63a5ce) SHA1(1d18606fbb28b55a921fc37e1af1aff4caae9003) )
 
 
-	ROM_REGION(0x100,           "user5", 0)
+	ROM_REGION(0x100,           REGION_USER5, 0)
 	ROM_LOAD("spc700.rom", 0, 0x40, CRC(44bb3a40) SHA1(97e352553e94242ae823547cd853eecda55c20f0) )
 
-	ROM_REGION(0x800,           "user6", ROMREGION_ERASEFF)
+	ROM_REGION(0x800,           REGION_USER6, ROMREGION_ERASEFF)
 
 ROM_END
 
